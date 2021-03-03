36cef849c4fdf58f062eef9b369311ee73348b93
ProdIsMyDev <chris@casualcompany.de>
2021-03-03 10:35:30 +0100

    Adding basic structure
    Signed-off-by: ProdIsMyDev <chris@casualcompany.de>

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='sentiment-analysis',
+    version='0.0.1',
+    description='Sentiment analysis for the german language',
+    long_description=readme,
+    author='Christoph Koenning',
+    author_email='chris@casualcompany.de',
+    url='https://github.com/Covidioten/BAPraktikumSentimentAnalyse',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
