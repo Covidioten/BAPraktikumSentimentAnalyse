2262daec849c58aa93bf308ed75da9e9114eafd0
ProdIsMyDev <chris@casualcompany.de>
2021-03-03 10:34:07 +0100

    moving example
    Signed-off-by: ProdIsMyDev <chris@casualcompany.de>

--- start_hp_job.sh
+++
@@ -1,1 +0,0 @@
-hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar -files tokenmapper.py,sumreducer.py -mapper tokenmapper.py -reducer sumreducer.py -input /data/twitter2020 -output twitter_wc
