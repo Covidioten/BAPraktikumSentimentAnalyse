1e916844dcfcca4e93c4848c09c89c43ed94170e
ProdIsMyDev <chris@casualcompany.de>
2021-03-03 10:41:59 +0100

    Removing licence
    Signed-off-by: ProdIsMyDev <chris@casualcompany.de>

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='sentiment-analysis',
     version='0.0.1',
