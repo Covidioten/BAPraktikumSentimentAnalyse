e3071034fc70a5676d2d5a6b3ee78f4b25ba83b3
Johannes Idelberger <9idelber@informatik.uni-hamburg.de>
2021-03-04 22:38:58 +0100

    * added functionality to filter data and output correct json format *

---
+++ sentiment_analysis/sumreducer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import sys
+from textblob_de import TextBlobDE as TextBlob
+import nltk
+nltk.download('punkt')
+
+(last_date, sum) = (None, 0)
+temp_string = ""
+counter = 0
+(last_id, last_time, last_text, last_senti, last_avg_senti) = (0, None, None, None, None)
+sum_string = ""
+print("{'root':[")
+
+for line in sys.stdin:
+    try:
+        (id, date, val, time, text) = line.strip().split("\t")
+        senti = TextBlob(text)
+        avg_senti = TextBlob(text)
+
+
+
+        if last_date and last_date != date:
+            print( "{'%s':{'total':'%s', 'sentiment':'%s', 'data':{ %s}}}," % (last_date, sum, avg_senti.sentiment.polarity, sum_string))
+            sum_string = "'%s':{'%s':'%s'}" % (id, time, senti.sentiment.polarity)
+            (last_date, sum) = (date, int(val))
+
+        elif last_date:
+            sum_string += ",'%s':{'%s':'%s'}" % (id, time, senti.sentiment.polarity)
+            (last_date, sum) = (date, sum+int(val))
+            # (last_id, last_time, last_senti, sum_string) = (id, time, senti, )
+        else:
+            sum_string += "'%s':{'%s':'%s'}" % (id, time, senti.sentiment.polarity)
+            (last_date, sum) = (date, sum+int(val))
+            # (last_id, last_time, last_senti, sum_string) = (id, time, senti, )
+    except ValueError:
+        pass
+
+if last_date:
+    print( "{'%s':{'total':'%s', 'sentiment':'%s', 'data':{ %s}}}" % (last_date, sum, avg_senti.sentiment.polarity, sum_string))
+    # print("}}")
+    print("]}")
---
+++ sentiment_analysis/tokenmapper.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import sys
+import json
+import re
+import datetime
+
+city_arr = ["Berlin", "Hamburg", "München", "Köln", "Frankfurt am Main",
+            "Stuttgart", "Düsseldorf", "Dortmund", "Essen", "Leipzig",
+            "Bremen", "Dresden", "Hannover", "Nürnberg", "Duisburg",
+            "Bochum", "Wuppertal", "Bielefeld",	"Bonn", "Münster"]
+
+bland_arr = ["Baden-Württemberg", "Bayern", "Brandenburg", "Hessen",
+            "Mecklenburg-Vorpommern", "Niedersachsen", "Nordrhein-Westfalen",
+             "Rheinland-Pfalz", "Saarland", "Sachsen"]
+
+land_arr = ["Germany", "Deutschland"]
+
+context_arr = ["lockdown", "covid", "corona"]
+
+def search_data(kw_array, text):
+    '''
+    Iterates over an array of keywords and checks for each keyword if its present
+    @param kw_array - the array of keywords to search for
+    @param text - the text to search in
+    @return - true: at least 1 of the keywords is in the text
+            - false: none of the keywords are in the text
+    '''
+    for entry in kw_array:
+        if re.search(entry, text, re.IGNORECASE):
+            return True
+
+    return False
+
+
+
+for line in sys.stdin:
+    items = json.loads(line)
+    try:
+
+        # ["created_at"]
+        # ["id"]
+        # ["text"]
+        # ["user"]["location"]
+        # ["user"]["id"]
+        # ["geo"]
+        # ["entities"]["hashtags"][<index>]["text"]
+        # ["lang"]
+
+        location = str(items["user"]['location'])
+        text = str(items["text"])
+        time = datetime.datetime.strptime(items["created_at"], "%a %b %d %H:%M:%S +0000 %Y")
+        tweet_id = items["id"]
+
+        # folgende annahmen werden getroffen:
+        #    - ein Bundesland kann nur dann richtig erkannt werden wenn es
+        #      ausgeschrieben wurde, "MV" als Abkürzung für Mecklenburg Vorpommern
+        #      könnte Beispielsweise auch in einem anderen Kontext vorkommen
+        #    - Länder analog "DE" für Deutschland könnte auch für etwas anderes stehen
+        # check if user has specified a city in the set of the 20 most populated german cities
+
+        if search_data(city_arr, location) | search_data(land_arr, location) | search_data(bland_arr, location):
+            # if search_data(context_arr, text):
+            print("%s\t%s\t%s\t%s\t%s" %  (tweet_id, time.date(), 1, time.time(), text))
+
+    except KeyError:
+        pass
