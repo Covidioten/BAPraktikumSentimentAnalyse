ca26ed20e2fc9900b3b82194aa966d459f66b99c
joe-hannes <65351662+joe-hannes@users.noreply.github.com>
2021-04-09 12:25:10 +0200

    added test cases for negative and neutral statements

--- tests/test_sentiment_analysis.py
+++ tests/test_sentiment_analysis.py
@@ -12,6 +12,14 @@ class BasicTestSuite(unittest.TestCase):
     def test_sentence_with_positive_sentiment(self):
         self.assertGreater(sentiment_analysis.get_sentiment_of_string(
             "Der Lockdown ist gut"), 0)
+        
+    def test_sentence_with_negative_sentiment(self):
+        self.assertLess(sentiment_analysis.get_sentiment_of_string(
+        "Der Lockdown ist nicht gut"), 0)
+        
+    def test_sentence_with_negative_sentiment(self):
+        self.assertEqual(sentiment_analysis.get_sentiment_of_string(
+        "Der Himmel ist blau"), 0)
 
 
 if __name__ == '__main__':
